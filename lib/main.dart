7c3308d879399b90d87232fc767a19c8928f745f
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2024-06-16 20:07:18 +0900

    About Flutter Layout 1. Widget 상 하 좌 우 Scroll 기능

--- lib/main.dart
+++ lib/main.dart
@@ -16,42 +16,54 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      child: Column(
+    return SingleChildScrollView( // Vertical Scroll View
+      scrollDirection: Axis.horizontal, // 가로 scroll view 변경 - child Row 변경
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 100,
-                height: 80,
-                color: Colors.red,
-                child: Text('Container 1'), // child 는 마지막에 위치하도록 권장..
-              ),
-              Container(
-                width: 100,
-                height: 80,
-                color: Colors.green,
-                child: Text('Container 2'),
-              ),
-              Container(
-                width: 100,
-                height: 80,
-                color: Colors.blue,
-                child: Text('Container 3'),
-              )
-            ],
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
           ),
           Container(
-            width: 300,
-            height: 120,
             color: Colors.grey,
-            child: Text('Container 4'),
-          )
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+          ),
         ],
       ),
     );
