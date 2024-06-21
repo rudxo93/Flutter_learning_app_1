b604f175c89f758a8c2e97ef3f71730cd6777c01
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2024-06-21 23:12:25 +0900

    About Flutter Layout 1. Widget 비율로 배치하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
+// Widget 비율로 배치하기
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text('Widget을 상 하 좌 우로 배치하기'),
-      ),
-      body: Body(),
-    ),
+          backgroundColor: Colors.blue.shade200,
+          title: const Text('Widget 비율로 배치하기')),
+      body: const Body2(),
+    ), // Scaffold - 기본, 도화지
   ));
 }
 
@@ -16,56 +19,28 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView( // Vertical Scroll View
-      scrollDirection: Axis.horizontal, // 가로 scroll view 변경 - child Row 변경
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Container(
-            color: Colors.grey,
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-          ),
-          Container(
-            color: Colors.grey,
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-          ),
-          Container(
-            color: Colors.grey,
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-          ),
-          Container(
-            color: Colors.grey,
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-          ),
-          Container(
-            color: Colors.grey,
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-          ),
-          Container(
-            color: Colors.grey,
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-          ),
-          Container(
-            color: Colors.grey,
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-          ),
-        ],
-      ),
+    return Column(
+      children: [ // 높이 지정안하고 Flexible로 감싸주면 최대한도로 알아서 1:1 높이 셋팅, flex 로 비율 셋팅도 가능
+        Flexible(flex:1, child: Container(color: Colors.red)),
+        Flexible(flex:2, child: Container(color: Colors.orange)),
+        Flexible(flex:1, child: Container(color: Colors.yellow)),
+        Flexible(flex:2, child: Container(color: Colors.green))
+      ],
+    );
+  }
+}
+
+class Body2 extends StatelessWidget {
+  const Body2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(width: double.infinity, height: 200, color: Colors.pink), // 남은 공간 모두 차지하게 - Flexible, Expanded 사용
+        Expanded(child: Container(color: Colors.yellow, height: 100)), // Expanded는 높이지정 상관 X 나머지 공간 차지
+        Flexible(child: Container(color: Colors.purple, height: 100))  // Flexible은 높이 지정할 경우 지정된 높이로 설정되어 나머지 공간 차지 X
+      ],
     );
   }
 }
